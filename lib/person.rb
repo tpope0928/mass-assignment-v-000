9a1bcf16efb8f607517c8c27d549d94e045e6dbf
Thomas Pope <tpope0928@gmail.com>
2018-09-12 13:10:51 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,10 @@
 class Person
-  
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
-
   def initialize(attributes=nil)
     if attributes
-      attribute.each do |k,v|
+      attributes.each do |k,v|
         self.send("#{k}=", v)
       end
     end
-    
   end
 end
